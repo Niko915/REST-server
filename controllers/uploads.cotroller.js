4b7574320093600b35c0f6844d5f164803065711
Nico <xniko915x@gmail.com>
2023-02-20 12:09:11 +0100

    Paquetes express-fileupload y uuid añadidos

--- controllers/uploads.cotroller.js
+++ controllers/uploads.cotroller.js
@@ -1,13 +1,42 @@
+const { v4: uuidv4 } = require('uuid');
+const path= require('path')
+
 const { response } = require("express");
 
 const cargarArchivos=(req,res=response)=>{
 
-    console.log(req.files)
+    if (!req.files || Object.keys(req.files).length === 0) {
+      res.status(400).json({msg:'No hay archivos que subir'});
+      return;
+    }
+  
+    if (!req.files.archivo) {
+        res.status(400).json({msg:'No hay archivos que subir'});
+        return;
+    }  
+ 
+    const {archivo} = req.files;
+    const nombreCortado = archivo.name.split('.');
+    const extension = nombreCortado[nombreCortado.length-1];
 
-    res.json({
-        msg:'Holas'
-    })
-
+    //validar la extensión
+    const extensionesValidas=['png','jpg','jpeg','gif','pdf','txt']
+    if (!extensionesValidas.includes(extension)) {
+      return res.status(400).json({
+        msg:`La extensión ${extension}, no está permitida`
+      })
+    }
+  
+   const nombreTemp = uuidv4()+'.'+extension;
+   const uploadPath =path.join( __dirname, '../uploads/',nombreTemp);
+  
+   archivo.mv(uploadPath, function(err) {
+      if (err) {
+        return res.status(500).json({err});
+      }
+  
+      res.json({msg:'File uploaded to ' + uploadPath});
+    });
 
 }
 
